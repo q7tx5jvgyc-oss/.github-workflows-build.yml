109b31a0c3496b103681dc4ab2e2febc301e0414
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-02 02:52:30 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,83 +1,31 @@
 #import <UIKit/UIKit.h>
-#import <IOKit/IOKitLib.h>
 
-// دوال التحكم باللمس
-void simulateTap(CGPoint point) {
-    // كود إرسال حدث اللمس للنظام (IOHIDEvent)
-    // يتطلب مهارة عالية في ربط الأحداث
-}
-
-// إنشاء "هدف" (Target) قابل للتحريك
-@interface DragView : UIView
+@interface DragButton : UIButton
 @end
 
-@implementation DragView {
-    CGPoint lastLocation;
-}
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    [self.superview bringSubviewToFront:self];
-    lastLocation = [[touches anyObject] locationInView:self.superview];
-}
+@implementation DragButton
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    CGPoint newPoint = [[touches anyObject] locationInView:self.superview];
-    self.center = CGPointMake(self.center.x + (newPoint.x - lastLocation.x), 
-                              self.center.y + (newPoint.y - lastLocation.y));
-    lastLocation = newPoint;
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.superview];
+    self.center = location;
 }
 @end
-UIWindow *menuWindow;
-UISlider *speedSlider;
-BOOL isRunning = NO;
 
-// إنشاء نافذة القائمة
-void showMenu() {
-    menuWindow = [[UIWindow alloc] initWithFrame:CGRectMake(50, 100, 200, 250)];
-    menuWindow.backgroundColor = [UIColor blackColor];
-    menuWindow.layer.cornerRadius = 10;
-    
-    // شريط السرعة
-    speedSlider = [[UISlider alloc] initWithFrame:CGRectMake(10, 50, 180, 30)];
-    speedSlider.minimumValue = 0.1;
-    speedSlider.maximumValue = 2.0;
-    [menuWindow addSubview:speedSlider];
-    
-    // زر البدء
-    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [startBtn setTitle:@"Start Clicker" forState:UIControlStateNormal];
-    startBtn.frame = CGRectMake(10, 100, 180, 40);
-    [menuWindow addSubview:startBtn];
-    
-    [menuWindow makeKeyAndVisible];
-}
-NSTimer *clickTimer;
-
-void startClicking(float speed) {
-    if (clickTimer) [clickTimer invalidate];
-    // التكرار بناءً على السرعة
-    clickTimer = [NSTimer scheduledTimerWithTimeInterval:speed 
-        repeats:YES block:^(NSTimer *timer) {
-        // هنا يتم استدعاء دالة simulateTap(lastLocation)
-    }];
-}
-
-void stopClicking() {
-    [clickTimer invalidate];
-    clickTimer = nil;
-}
-NSMutableArray *targets = [[NSMutableArray alloc] init];
-
-void addNewTarget(CGPoint location) {
-    DragView *newTarget = [[DragView alloc] initWithFrame:CGRectMake(location.x, location.y, 50, 50)];
-    newTarget.backgroundColor = [UIColor redColor]; // شكل الهدف
-    [[UIApplication sharedApplication].keyWindow addSubview:newTarget];
-    [targets addObject:newTarget];
-}
 %hook SpringBoard
 - (void)applicationDidFinishLaunching:(id)application {
     %orig;
-    // تأخير بسيط لضمان تحميل واجهة المستخدم
+
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        showMenu();
+        UIWindow *win = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        win.windowLevel = UIWindowLevelAlert + 1;
+        win.hidden = NO;
+        win.backgroundColor = [UIColor clearColor];
+
+        DragButton *btn = [DragButton buttonWithType:UIButtonTypeSystem];
+        btn.frame = CGRectMake(100, 100, 60, 60);
+        btn.backgroundColor = [UIColor redColor];
+        [btn setTitle:@"Auto" forState:UIControlStateNormal];
+        [win addSubview:btn];
     });
 }
 %end
