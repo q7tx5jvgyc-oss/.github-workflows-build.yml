54a34f363844800df6f6c3e9f8b379a49a73f21c
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-02 02:09:29 +0300

    Update main.mm

--- .github/workflows/main.mm
+++ .github/workflows/main.mm
@@ -1,12 +1,8 @@
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 %hook SpringBoard
 - (void)applicationDidFinishLaunching:(id)application {
     %orig;
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Moustache" 
-        message:@"Tweak Loaded Successfully!" 
-        preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+    NSLog(@"Moustache: Tweak Loaded Successfully!");
 }
 %end
